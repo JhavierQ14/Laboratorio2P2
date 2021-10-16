1d1e8ab05d85b2b593bc308cbd09ceb4fa197ff3
carlosjavierjimenezvasquez <69540480+carlosjavierjimenezvasquez@users.noreply.github.com>
2021-10-16 17:42:50 -0600

    conexion bd

---
+++ Laboratorio2/Laboratorio2/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Laboratorio2.Entidad;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio2.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+
+
+        }
+        
+
+
+        
+        public DbSet<persona> personas { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
+}
---
+++ Laboratorio2/Laboratorio2/Entidad/persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio2.Entidad
+{
+    public class persona
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int idPersona { get; set; }
+
+        public string NombrePersona { get; set; }
+
+        public int EdadPersona { get; set; }
+
+        public string DescripcionPersona { get; set; }
+    }
+}
